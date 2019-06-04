f63c2871d2ecae93c139d9aead9a9165df8e2cd4
Rob Tamlyn <MemoryChips@yahoo.com>
2019-06-04 13:41:05 -0400

    Adds demo version define in app.cpp

--- sandbox/app.cpp
+++ sandbox/app.cpp
@@ -1,10 +1,13 @@
 #include "app.h"
 
+#define V6_DEMO_VERSION "0.0.1"
+
 int main(int argc, char **argv) {
 
   v6::Log::init(); // can this be done in run()?
   LOG_CORE_WARN("Initialized log");
   LOG_INFO("Hello from client application");
+  LOG_INFO("Demo version: {0}", V6_DEMO_VERSION);
   LOG_CORE_ERROR("an error: {0}", "that needs to be logged");
   LOG_CORE_TRACE("a trace message: {0}", "that could be ignored");
 
