d21797721f295154fe41b98361e674afc3f5ee14
Rob Tamlyn <MemoryChips@yahoo.com>
2019-10-25 07:48:01 -0400

    Adds cleanup

--- sandbox/src/sandbox-2d.cpp
+++ sandbox/src/sandbox-2d.cpp
@@ -10,12 +10,6 @@ Sandbox2D::Sandbox2D()
 
 void Sandbox2D::onDetach() {}
 
-void Sandbox2D::onImGuiRender() {
-  ImGui::Begin("Settings");
-  ImGui::ColorEdit4("Square Color", glm::value_ptr(squareColor));
-  ImGui::End();
-}
-
 void Sandbox2D::onEvent(v6::Event &e) { orthoCamController.onEvent(e); }
 
 void Sandbox2D::onAttach() {}
@@ -31,12 +25,10 @@ void Sandbox2D::onUpdate(Timestep ts) {
   v6::Renderer2D::beginScene(orthoCamController.GetCamera());
   v6::Renderer2D::drawQuad({0.0f, 0.0f}, {1.0, 1.0f}, {0.8f, 0.1f, 0.1f, 1.0f});
   v6::Renderer2D::endScene();
+}
 
-  // TODO: Shader::setMat4 Shader::setFloat4
-  // std::dynamic_pointer_cast<v6::OpenGLShader>(pFlatColorShader)->bind();
-  // std::dynamic_pointer_cast<v6::OpenGLShader>(pFlatColorShader)->UploadUniformFloat4("u_Color",
-  // squareColor);
-
-  // glm::mat4 scale2 = glm::scale(glm::mat4(1.0f), glm::vec3(1.5f));
-  // v6::Renderer::Submit(pFlatColorShader, pSquareVA, scale2);
+void Sandbox2D::onImGuiRender() {
+  ImGui::Begin("Settings");
+  ImGui::ColorEdit4("Square Color", glm::value_ptr(squareColor));
+  ImGui::End();
 }
