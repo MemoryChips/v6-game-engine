112f9f3a98553e505c1935564421afd033197284
Rob Tamlyn <MemoryChips@yahoo.com>
2019-12-20 19:01:51 -0500

    Adds graphics context

---
+++ v6/src/renderer/graphics-context.cpp
@@ -0,0 +1,21 @@
+#include "renderer/graphics-context.h"
+
+#include "platform/opengl/opengl-context.h"
+#include "renderer/renderer.h"
+
+namespace v6 {
+
+Scope<GraphicsContext> GraphicsContext::Create(void *window) {
+  switch (Renderer::GetAPI()) {
+  case RendererAPI::API::None:
+    V6_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
+    return nullptr;
+  case RendererAPI::API::OpenGL:
+    return CreateScope<OpenGLContext>(static_cast<GLFWwindow *>(window));
+  }
+
+  V6_CORE_ASSERT(false, "Unknown RendererAPI!");
+  return nullptr;
+}
+
+} // namespace v6
--- v6/src/renderer/graphics-context.h
+++ v6/src/renderer/graphics-context.h
@@ -1,11 +1,14 @@
 #pragma once
 
+#include "v6-pch.h"
+
 namespace v6 {
 
 class GraphicsContext {
 public:
   virtual void Init() = 0;
   virtual void SwapBuffers() = 0;
+  static Scope<GraphicsContext> Create(void *window);
 };
 
 } // namespace v6
